4e1763263f5c8dc03da9207f3097206336762ded
Laura Torres <ltorresfdz@gmail.com>
2020-10-31 15:39:55 -0600

    table uploaded

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+  // // Use d3 to update each cell's text with
+  // ufo values (datetime, city, state,country,shape,duration, comments)
+  data.forEach(function(ufo) {
+    console.log(ufo);
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
